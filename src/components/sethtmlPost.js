43c485c8ed57d27e6a7f7b00f86be6f293963114
Karen Tapia <karenina.tap@gmail.com>
2021-03-17 11:52:57 -0600

    last changes, delete is working

--- src/components/sethtmlPost.js
+++ src/components/sethtmlPost.js
@@ -1,3 +1,4 @@
 export function setHtml(elements){
-    return `<h1>${elements.post}</h1>`
+    return `<div id="containerPost">
+<i class="material-icons">article</i><h1>${elements.post}</h1><span class="icons"><i class="material-icons">description</i><i class="material-icons">delete</i></span></div>`
 }
--- src/post.js
+++ src/post.js
@@ -2,61 +2,87 @@ import { setHtml } from './components/sethtmlPost.js'
 
 export const postPage = (container) => {
   const html = `<div id="post-input">
-  <label for="post-area">Post</label>
-  <input type="text" id="post-area" />
-  <button id="post-btn">Publicar</button>
-  </div>
-  <div id="posts"></div>`;
-    
+  <form id="formulario">
+  <input type="text" id="post-text">
+  <button type="submit">Publicar</button>
+</form> </div>
+<div id="listaPost"></div>`;
+
   container.innerHTML = html
-  //Array donde se van a guardar los nuevos elementos "post"
-  let postList = JSON.parse(localStorage.getItem("posting"));
-  if (postList == null) {
-    postList = []
-  } /* else {
-    
-  } */
-  const imprime = () => {
-    const containersin = document.getElementById("posts");
-    postList.forEach(posteo => {
-      containersin.innerHTML += `<h1>${posteo.post}</h1>`
-    })
-    /*   const arrayElementos= postList.map (function (unPost) {
-        return `<h1>${elements.post}</h1>`
-      }); */
 
-    /*     postList.forEach(element => {
-        container2.innerHTML += setHtml(element)
-      }); */
-  }
+  //Variables GLobales
+  //Acá guarda el formulario 
+  const formularioUI = document.getElementById('formulario');
+  //Aqui guarda lo recibido en la seccion donde se imprimen las actividades
+  const showPost = document.getElementById('listaPost');
+  //Aqui se almacena todo lo guardado por el usuario en un "Array" no necesita llamarse "array"
+  let savePost = [];
 
-
-  //Aca guarda los manda al array
-  const createPost = (post) => {
+  //Funciones 
+  //En esta funcion "Crear Item" se van creando las cosas que entraran dentro del Array vacio, 
+  //cada que creemos una actividad necesitamos mandar un parametro, en este caso "actividad"
+  const crearItem = (post) => {
     let item = {
       post: post,
     }
-    postList.push(item);
+    savePost.push(item);
     return item;
   }
-  //Esta funcion guarda los post en el local
-  const createdPost = () => {
-    localStorage.setItem('posting', JSON.stringify(postList));
-    //console.log(postList);
-    imprime();
 
+  const saveDB = () => {
+    //Aqui rutina es unicamente un identificador
+    localStorage.setItem('recentPost', JSON.stringify(savePost))
+    printDB();
   }
 
-  //Funcion del boton
-  const postBtn = document.getElementById('post-btn');
-  postBtn.addEventListener('click', (event) => {
-    console.log("El click funciona");
-    event.preventDefault();
-    event.stopPropagation();
-    //Aca trae el texto del input
-    let newText = document.getElementById('post-area').value;
-    createPost(newText);
-    createdPost();
+  const printDB = () => {
+    showPost.innerHTML = '';
+    savePost = JSON.parse(localStorage.getItem('recentPost'));
+    if (savePost === null) {
+      savePost = [];
+    } else {
+      savePost.forEach(element => {
+        showPost.innerHTML += setHtml(element) 
+      });
+    }
+  }
+
+  const deleteDB = (post) => {
+    let indexArray;
+    savePost.forEach((elemento,index) => {
+      //Aqui se puede utilizar Find Index
+      if(elemento.post === post){
+       indexArray=index;
+      }
+    }
+  );
+  savePost.splice(indexArray,1);
+  saveDB()
+  }
+
+
+  //EventListener
+
+  formularioUI.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let actividadUI = document.querySelector('#post-text').value;
+    //Aqui crear Item recibe lo almacenado en actividadIU o en el id actividad del input del formulario
+    crearItem(actividadUI);
+    saveDB();
+    formularioUI.reset();
+  });
+
+  document.addEventListener('DOMContentLoaded', printDB);
+
+  showPost.addEventListener('click', (e) =>{
+    e.preventDefault();
+     if(e.target.innerHTML === 'delete' || e.target.innerHTML === 'description'){
+      let texto=e.path[2].childNodes[2].innerHTML;
+      if(e.target.innerHTML === 'delete'){
+        deleteDB(texto);
+
+      }
+    } 
   })
 
 };
